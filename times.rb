5350b811c03e571e1cdac91bdc8ac36cfe03031f
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-20 05:10:30 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,12 +1,12 @@
 def using_times
 	#your code here
 
-using_times = 100 
+# using_times = 100 
 
-7.times do
+# 7.times do
   
-puts "Wingardium Leviosa"	
-using_times = using_times - 1 	
+# puts "Wingardium Leviosa"	
+# using_times = using_times - 1 	
 	
 end
 
